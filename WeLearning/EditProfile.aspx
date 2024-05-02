9de5bd42efdd6fd06e145d71f2abde5acf1dbbc9
Avinash Ajay <avi.ajay6@gmail.com>
2024-05-02 11:28:30 +0400

    made an edit profile page where you can update the password of the user, set up the link in the master site too

---
+++ WeLearning/EditProfile.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="EditProfile.aspx.cs" Inherits="WeLearning.EditProfile" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        Edit Profile
+                    </div>
+                    <div class="card-body">
+                        <asp:Label ID="lblStatus" runat="server" CssClass="text-success" Visible="false"></asp:Label>
+                        <div class="form-group">
+                            <label for="txtUsername">Username:</label>
+                            <asp:TextBox ID="txtUsername" runat="server" CssClass="form-control" ReadOnly="true" />
+                        </div>
+                        <div class="form-group">
+                            <label for="txtPassword">New Password:</label>
+                            <asp:TextBox ID="txtPassword" runat="server" TextMode="Password" CssClass="form-control" />
+                        </div>
+                        <asp:Button ID="btnSave" runat="server" Text="Update Password" CssClass="btn btn-primary" OnClick="btnSave_Click" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ WeLearning/EditProfile.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+
+namespace WeLearning
+{
+    public partial class EditProfile : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                MembershipUser currentUser = Membership.GetUser();
+                if (currentUser != null)
+                {
+                    txtUsername.Text = currentUser.UserName;
+                }
+            }
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            MembershipUser currentUser = Membership.GetUser();
+            if (currentUser != null)
+            {
+                string newPassword = txtPassword.Text.Trim();
+                if (!string.IsNullOrEmpty(newPassword))
+                {
+                    string resetPassword = currentUser.ResetPassword();
+                    bool changePasswordSucceeded = currentUser.ChangePassword(resetPassword, newPassword);
+
+                    if (changePasswordSucceeded)
+                    {
+                        lblStatus.Text = "Password updated successfully.";
+                        lblStatus.CssClass = "text-success";
+                        lblStatus.Visible = true;
+                    }
+                    else
+                    {
+                        lblStatus.Text = "Password update failed. Please re-enter your values and try again.";
+                        lblStatus.CssClass = "text-danger";
+                        lblStatus.Visible = true;
+                    }
+                }
+            }
+        }
+    }
+    
+}
